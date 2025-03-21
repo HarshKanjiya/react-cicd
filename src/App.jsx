3303e1415a8e11a41ba786827cfeba95908e331a
Harsh Kanjiya <harshkanjiya001@gmail.com>
2025-03-21 23:58:42 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} is it really working?
+          count is {count} is it really working? lalala
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
